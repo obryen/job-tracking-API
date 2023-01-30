735d4a23a8414cc8b572dcccc75c1a61b7cbce93
Bryan <bryantoromo@gmail.com>
2023-01-30 15:58:05 +0300

    wire all endpoints to the app, handle any unexpected errors from code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { sequelize } = require("./model");
 const app = express();
 const router = new Router();
+const util = require("util");
 
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
+app.use(router);
+
+app.use((error, req, res, next) => {
+  if (error instanceof HttpError) {
+    return res.status(error.code).json({
+      error: error.message,
+    });
+  }
+
+  console.error("unexpected behavior:", util.inspect(error));
+
+  res.status(500).json({
+    error: "Internal server error",
+  });
+});
 
 module.exports = app;
